eb82e1eff79a51f0de9f32faf6b05a8b0c36969f
Harrison Brown <harrisonbro@gmail.com>
2013-01-21 17:29:06 +0000

    Added line to HEAD about prefetching individual assets
    See CSS Wizardry's "Front-end performance for web designers and front-end developers" article.

--- header.php
+++ header.php
@@ -4,6 +4,8 @@
   
   <!-- Pre-fetch domains of assets requested later in the document -->
   <link rel="dns-prefetch" href="//example.tld">
+  <!-- Pre-fetch assets globally-used assets (but *not* JavaScript) -->
+  <link rel="prefetch" href="asset.ext">
   
   <!-- Prevent some browsers (eg. Chrome) attempting to pre-fetch all URLs on your page -->
   <!-- See: http://www.buyog.com/code/?id=53 -->
